08a993ff5411539b5628a7d071a871c4cb9db9f0
Sarkis <mkrtc.sarkis@gmail.com>
2025-06-10 01:39:10 +0300

    patch: canvas-event.entity.ts

--- server/src/canvas-event/entities/canvas-event.entity.ts
+++ server/src/canvas-event/entities/canvas-event.entity.ts
@@ -1,37 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { CanvasEventEnum, CanvasEventPayload } from '../types';
 
-// export type CanvasEventType = 'create' | 'move' | 'clear' | 'remove';
-
-// interface CreateFigureEvent {
-//   id: string;
-//   x: number;
-//   y: number;
-//   color: string;
-//   target: "square"; // "square" | "circle" | "triangle"
-//   type: "create";
-// }
-
-// interface MoveEvent{
-//   type: "move";
-//   id: string;
-//   x: number;
-//   y: number;
-// }
-
-// interface ClearEvent{
-//   type: "clear";
-// }
-
-// interface RemoveEvent{
-//   type: "remove";
-//   id: string;
-// }
-
-// export type CanvasEventPayload = CreateFigureEvent | MoveEvent | ClearEvent | RemoveEvent;
-
-// export type EventPayloadByType<T extends CanvasEventType> = Extract<CanvasEventPayload, {type: T}>;
-
 @Entity('events')
 export class CanvasEventEntity {
   @PrimaryGeneratedColumn('uuid')
